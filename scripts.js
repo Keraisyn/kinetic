6d48a8dd2b47a731d043598e25c99c9db3dc99ec
Rohan Shetty <35475826+LessKinetik@users.noreply.github.com>
2018-01-31 11:39:24 -0500

    General release update

--- scripts.js
+++ scripts.js
@@ -7,7 +7,9 @@ function background() {
     "/backgrounds/background_04.jpeg",
     "/backgrounds/background_05.jpeg",
     "/backgrounds/background_06.jpeg",
-    "/backgrounds/background_07.jpeg"
+    "/backgrounds/background_07.jpeg",
+    "/backgrounds/background_08.jpeg",
+    "/backgrounds/background_09.jpeg"
   ];
   
   image = "url(" + backgroundImages[Math.floor(Math.random() * backgroundImages.length)] + ")";
@@ -44,31 +46,41 @@ function clock() {
 function calendar() {
   document.getElementById("calendmodal").style.display = "block";
 }
-
-function closeCalendar(e) {
-  if (e.target === this) {
-    this.style.display = "none";
-  }
-}
-
 // Settings Button
 function linksModal() {
   document.getElementById("linksmodal").style.display = "block";
 }
 
-function closeLinks(e) {
-  if (e.target === this) {
-    this.style.display = "none";
-  }
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("hideAll");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 100);
+    fadeTarget.style.display = "none";
 }
 
 /* Main */
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("calendicon").addEventListener("click", calendar);
-  document.getElementById("linksicon").addEventListener("click", linksModal);
-  document.getElementById("calendmodal").addEventListener("click", closeCalendar);
-  document.getElementById("linksmodal").addEventListener("click", closeLinks);
   background();
   clock();
-  
+  fadeOutEffect();
+  document.getElementById("calendicon").addEventListener("click", calendar);
+  document.getElementById("linksicon").addEventListener("click", linksModal);
+  document.getElementById("calendmodal").addEventListener("click", function(event){
+    if (event.target !== this)
+    return;
+    this.style.display = "none";
+  });
+  document.getElementById("linksmodal").addEventListener("click", function(event){
+    if (event.target !== this)
+    return;
+    this.style.display = "none";
+  });
 });
